4221d30cea3ed53dcd7db4fa2975dec24a45954b
Jason Dusek <jason.dusek@gmail.com>
2016-04-07 19:23:38 -0700

    Bug fix release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 conf = dict(name='magiclog',
-            version='0.99',
+            version='0.99.1',
             author='Jason Dusek',
             author_email='jason.dusek@gmail.com',
             url='https://github.com/drcloud/magiclog',
